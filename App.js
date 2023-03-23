0c202b67da550831b5490ee842504decdd92b5d3
ertyhd <orionship@protonmail.com>
2023-03-23 20:21:06 +0200

    second try

---
+++ App.js
@@ -0,0 +1,9 @@
+import RegistrationScreen from "./Screens/RegistrationScreen";
+import LoginScreen from "./Screens/LoginScreen";
+
+import React from "react";
+
+export default function App() {
+  return <RegistrationScreen />;
+  // return <LoginScreen />;
+}
